833571045291dfe3cb31a531e980e98a92a4b477
Bhavjyot10 <bhavjyot08@gmail.com>
2024-01-30 20:21:55 +0530

    bhav-wave-spawner

--- Assets/WaveSpawner.cs
+++ Assets/WaveSpawner.cs
@@ -1,68 +1,80 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class WaveSpawner : MonoBehaviour
 {
 
 	public int waveNumber = 0;
 	public int waveEnemyCount = 3;
-	public float startDelayTime = 1f;
+	private int enemyCount;
+	public float startDelayTime = 5f;
 	public float wavesDelayTime = 10f;
-	bool hasEnemyDeployed = false;
-	bool hasStarted = false;
+	
+	bool hasWaveStarted = false;
 	public float enemySpawnDelay = 1f;
+
+	public float innerRadius = 2f;
+	public float outerRadius = 5f;
 	
 
 	public List<GameObject> enemies = new List<GameObject>();
+
+	public GameObject Car;
+
+	public static event Action OnWaveStart;
     // Start is called before the first frame update
     void Start()
 	{
-		StartCoroutine(StartDelay(startDelayTime));
+		OnWaveStart += StartSpawningCoroutine;
+        StartCoroutine(StartDelay(startDelayTime));   
     }
 
 	// Update is called once per frame
 	void Update()
 	{
 		Debug.Log(waveNumber);
-		if (!hasStarted) return;
-		if (GameObject.FindWithTag("Enemy") == null)
+
+		if(GameObject.FindWithTag("Enemy") == null && hasWaveStarted)
 		{
-			endWave();
+			EndWave();
 		}
+		
     }
 
 	private IEnumerator StartDelay(float time_in_sec)
 	{
 		yield return new WaitForSeconds(time_in_sec);
-		hasStarted = true;
-		waveNumber++;
-        yield return SpawnEnemy(waveEnemyCount++, 2, 8, enemySpawnDelay);
+		hasWaveStarted = true;
+		waveNumber+=1;		
+		enemyCount = waveEnemyCount;
+		OnWaveStart?.Invoke();
     }
 
-	private IEnumerator SpawnEnemy(int count,int start, int end,float wait_time = 0)
-	{
-		for (int i = 0; i < count; i++)
+	private IEnumerator SpawnEnemy()
+	{    while (enemyCount != 0)
 		{
-			Vector2 randomLoation = getRandomLocation(start, end);
-			GameObject newEnemy = Instantiate(enemies[0], randomLoation, Quaternion.identity);
-            Destroy(newEnemy, 5f);
-            yield return new WaitForSeconds(wait_time);
-        }
+			float xPos = Random.Range(0, 2) == 0 ? Random.Range(-outerRadius, -innerRadius) : Random.Range(innerRadius, outerRadius);
+			float yPos = Random.Range(0, 2) == 0 ? Random.Range(-outerRadius, -innerRadius) : Random.Range(innerRadius, outerRadius);
+			Vector3 enemyPosOffset = new Vector2(xPos, yPos);
+			GameObject newEnemy = Instantiate(enemies[0], Car.transform.position + enemyPosOffset, Quaternion.identity);
+			Destroy(newEnemy, 5f);
+			enemyCount--;
+			yield return null;
+		}
     }
 
-	private void endWave()
+	private void EndWave()
 	{
-		hasStarted = false;
-		StartCoroutine(StartDelay(wavesDelayTime));
+		hasWaveStarted = false;
+        waveEnemyCount += 1;
+        StartCoroutine(StartDelay(wavesDelayTime));
 	}
-	 
-	private Vector2 getRandomLocation(int start,int end)
-	{
-        int spawnX = Random.Range(start, end);
-        if (Random.Range(0, 2) == 1) spawnX *= -1;
-        int spawnY = Random.Range(start, end);
-        if (Random.Range(0, 2) == 1) spawnY *= -1;
-		return new Vector2(spawnX, spawnY);
+    void StartSpawningCoroutine()
+    {
+        StartCoroutine(SpawnEnemy());
     }
+
 }
