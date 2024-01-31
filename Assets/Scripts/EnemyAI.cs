206c21bf74590354a48e2fd355361c045db8edc6
Bhavjyot10 <bhavjyot08@gmail.com>
2024-01-31 13:38:56 +0530

    basic enemy AI

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -5,23 +5,33 @@ using UnityEngine;
 public class EnemyAI : MonoBehaviour
 {
 
-    private Transform playerTransfrom;
-    public float speed = 20f;
-    private float duration = 1;
-    public float time = 10f;
-
-    // Start is called before the first frame update
+    public float moveSpeed = 5f;
+    public float rotationSpeed = 5f;
+    public float carStoppingDistance = 1.5f;
+    public Transform car;
+    
     void Start()
     {
-        playerTransfrom = GameObject.FindWithTag("Player").transform;
+        car = GameObject.FindWithTag("Player").transform;
     }
-
-    // Update is called once per frame
     void Update()
     {
-       
+        if (car != null)
+        {
+            Vector2 direction = car.position - transform.position;
 
 
+            // Lerp to smoothly rotate towards the player
+            transform.up = Vector2.Lerp(transform.up, direction.normalized, rotationSpeed * Time.deltaTime);
+            float distance = Vector2.Distance(transform.position, car.position);
+            if (distance <= carStoppingDistance)
+                return;
+            // Move the enemy towards the player
+            float step = moveSpeed * Time.deltaTime;
+            
+            transform.position = Vector2.MoveTowards(transform.position, car.position, step);
+            
+            
+        }
     }
-    
 }
